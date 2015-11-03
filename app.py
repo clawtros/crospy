9f5fad1adf37c7bd739e5bee82eb5ad9a818332d
Adam Benzan <adam.benzan@gmail.com>
2015-11-03 15:40:18 -0500

    ok let's put it all here

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.cors import CORS
 from generator.generator import get_random, get_random_orig
 import json
 
 app = Flask(__name__)
 CORS(app)
-app.debug = True
 
-@app.route('/size/<int:size>/')
+@app.route('/api/random/size/<int:size>/')
 def play_size(size):
     return json.dumps(get_random_orig(size=size))
 
 
+@app.route('/api/random/')
+def random():
+    return json.dumps(get_random())
+
+
 @app.route('/')
 def index():
-    return json.dumps(get_random())
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run()
