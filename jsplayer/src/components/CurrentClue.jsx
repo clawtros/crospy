0f8124508154c949486cb92f800e6696be481e03
Adam Benzan <adam.benzan@gmail.com>
2016-02-07 08:12:41 -0500

    faff about

--- jsplayer/src/components/CurrentClue.jsx
+++ jsplayer/src/components/CurrentClue.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 var DIRECTIONS = require('../models/Directions.js');
 
-export default React.createClass({
-  render: function() {
+export default (props) => {
     return (
-      <h3 className="current-clue">{this.props.clue.clue_number}{this.props.direction == DIRECTIONS.ACROSS ? 'A' : 'D'} <span className="current-clue-text">{this.props.clue.clue_text}</span></h3>
+      <h3 className="current-clue">
+        {props.clue.clue_number}{props.direction == DIRECTIONS.ACROSS ? 'A' : 'D'}
+        &nbsp;
+        <span className="current-clue-text">{props.clue.clue_text}</span>
+      </h3>
     );
-  }
-});
+}
